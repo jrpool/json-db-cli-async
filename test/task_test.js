73975e0691a2d4d4ba2158ce47fa028224a78f75
Jonathan Pool <pool@stulta.com>
2017-07-11 12:09:40 -0700

    Corrected errors in tests.

--- test/task_test.js
+++ test/task_test.js
@@ -5,40 +5,58 @@ let thisID;
 
 describe('task', function() {
 
+  beforeEach('factory reset before each test', function() {
+    execSync('node task done 1-9');
+    execSync('node task reset');
+  });
+
+  after('factory reset after final test', function() {
+    execSync('node task done 1-9');
+    execSync('node task reset');
+  });
+
   context('valid arguments', function() {
 
-    it('add command gets answer in correct format', function() {
+    it('first add command adds task 1', function() {
       const response = execSync(
         'node task add \'test the add module\''
       ).toString();
-      // EXPECT RESPONSE TO MATCH(/.../)
-      expect(/^Created task \d+\./.test(response)).true;
-      // Identify the ID of the just-added task.
-      thisID = response.replace(/[^\d]+/g, '');
+      expect(response).equal('Created task 1.\n');
     });
 
-    it('done command gets correct answer', function() {
+    it('second add command adds task 2', function() {
+      execSync('node task add \'test the add module once\'').toString();
       const response = execSync(
-        'node task done ' + thisID
+        'node task add \'test the add module again\''
       ).toString();
-      expect(response).equal(
-        'Completed the task \'' + thisID + ': ' + 'test the add module\'.\n'
-      );
+      expect(response).equal('Created task 2.\n');
+    });
+
+    it('done command on 1 task deletes it', function() {
+      execSync('node task add \'test done\'').toString();
+      execSync('node task add \'test done again\'').toString();
+      const response = execSync('node task done 2').toString();
+      expect(response).equal('Completed the task \'2: test done again\'.\n');
     });
 
     it('list command gets answer in correct format', function() {
-      const response = execSync(
-        'node task list'
-      ).toString();
-      //expect(/^.+ID.+Description.+ tasks?\..*$/m.test(response)).true;
-      expect(/^[^]+ID.+Description[^]+\d+ tasks?\.\n$/.test(response)).true;
+      execSync('node task add \'test list\'').toString();
+      execSync('node task add \'test list again\'').toString();
+      const response = execSync('node task list').toString();
+      expect(response).match(/^[^]+ID.+Description[^]+2 tasks\.\n$/);
+    });
+
+    it('reset command resets the list', function() {
+      execSync('node task add \'test reset\'').toString();
+      execSync('node task add \'test reset again\'').toString();
+      execSync('node task done 1-2').toString();
+      const response = execSync('node task reset').toString();
+      expect(response).equal('Next ID reset to 1\n');
     });
 
     it('help command gets answer in correct format', function() {
-      const response = execSync(
-        'node task help'
-      ).toString();
-      expect(/^\n=+\nThis application manages [^]+$/.test(response)).true;
+      const response = execSync('node task help').toString();
+      expect(response).match(/^\n=+\nThis application manages [^]+$/);
     });
 
   });
@@ -52,15 +70,37 @@ describe('task', function() {
       );
     });
 
-    it('done command with invalid ID range', function() {
+    it('done command with an ID range containing no tasks', function() {
       const response = execSync('node task done 999-1002').toString();
       expect(response).equal(
         'The removal was unsuccessful because no such task exists.\n'
       );
     });
 
+    it('done command with an ID range in descending order', function() {
+      const response = execSync('node task done 2-1').toString();
+      expect(response).equal(
+        'The command was unsuccessful because of a format error.\n'
+      );
+    });
+
+    it('done command with a nonnumeric ID', function() {
+      const response = execSync('node task done V').toString();
+      expect(response).equal(
+        'The command was unsuccessful because of a format error.\n'
+      );
+    });
+
+    it('reset command with tasks in existence', function() {
+      execSync('node task add \'test reset\'').toString();
+      const response = execSync('node task reset').toString();
+      expect(response).equal(
+        'Before a reset, you must report all tasks complete.\n'
+      );
+    });
+
     it('invalid command name', function() {
-      const response = execSync('node task kill').toString();
+      const response = execSync('node task kill 1').toString();
       expect(response).equal(
         'The command was unsuccessful because of a format error.\n'
       );
